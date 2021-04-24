4d5901820ff01d084108a74efb5d2e22b2432a12
cb0806151 <cb0806151@otc.edu>
2021-04-24 15:23:37 -0500

    test out wallet integration with reach

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("hello there");
+import 'regenerator-runtime/runtime'
+import { getDefaultAccount, formatCurrency, balanceOf} from '@reach-sh/stdlib/ETH';
+
+window.onload = async () => {
+
+    let account;
+    let balance;
+
+    window.connectWallet = async () => {
+        await getAccount();
+        await getBalance();
+    }
+
+    const getAccount = async () => {
+        account = await getDefaultAccount();
+        console.log(account);
+    }
+
+    const getBalance = async () => {
+        balance = formatCurrency(await balanceOf(account), 4);
+        console.log(balance);
+    }
+
+}
